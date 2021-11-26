2cbb797a0a00d376c4b18036d13e7c1f7ae300a5
lin-1211 <85057992+lin-1211@users.noreply.github.com>
2021-11-26 20:36:38 +0800

    Update 1126HW.py

--- 1126HW.py
+++ 1126HW.py
@@ -17,36 +17,93 @@ while True:
     
     sel = input('請輸入欲執行選項: ')
 
+    d = {}
+fo = open('vocabulary.txt', 'w')
+
+print('############################')
+print('#####歡迎進入英文高手系統######')
+print('############################')
+print('*** 使用本系統，你將成為英文高手!!! ***')
+
+while True:
+    print('=>')
+    print('1. 建立辭彙')
+    print('2. 列出所有單字')
+    print('3. 英翻中')
+    print('4. 中翻英')
+    print('5. 測驗學習成果')
+    print('6. 離開系統')
+    
+    sel = input('請輸入欲執行選項: ')
+
+ d = {}
+fo = open('vocabulary.txt', 'w')
+
+print('############################')
+print('#####歡迎進入英文高手系統######')
+print('############################')
+print('*** 使用本系統，你將成為英文高手!!! ***')
+
+while True:
+    print('=>')
+    print('1. 建立辭彙')
+    print('2. 列出所有單字')
+    print('3. 英翻中')
+    print('4. 中翻英')
+    print('5. 測驗學習成果')
+    print('6. 離開系統')
+    
+    sel = input('請輸入欲執行選項: ')
+
     if sel=='1':
-        print('1. 建立辭彙')
-        word = input('請輸入單字:')
-        if(word == '0'):
-            continue
-        else:
-            chinese = input('請輸入中文翻譯:')
-            vocabularies[word] = chinese
+        while True:
+            voc = input('輸入新單字(按0跳出): ')
+            if voc == '0':
+                break
+            if voc not in d:
+                m = input('輸入中文解釋: ')
+                d[voc] = m
+                fo.write(f'{voc}:{m}'.encode("utf-8").decode("utf-8"))
+            else:
+                print('單字已經存在')
     elif sel=='2':
-        print('2. 列出所有單字')
-        for k in vocabularies.keys():
-            print(f"{k} 的意思是 {vocabularies[k]}")
+        lk = sorted(d)
+        for item in lk:
+            print(item, '是', d[item])
     elif sel=='3': # 英翻中
-        print('3. 英翻中')
-        word = input('請輸入單字:')
-        if word in vocabularies.keys():
-            print(f"{word} 的意思是 {vocabularies[word]}")
+        voc = input('輸入要查詢的英文單字(按0跳出): ')
+        if voc == '0':
+            continue
+        if voc in d: 
+            print(d[voc])
         else:
-            print('查無此單字')
+            print('我的字典沒有這個單字')
     elif sel=='4': # 中翻英
-        print('4. 中翻英')
-        chinese = input('請輸入中文:')
-        for k,v in vocabularies.items():
-            if(v == chinese):
-              None
-            #腦袋秀逗，不知道怎麼做......
+        got=False
+        ch = input('輸入要查詢的中文(按0跳出): ')
+        if ch == '0':
+            continue
+        for k,v in d.items():
+            if ch==v:
+                print(ch,'的英文是',k)
+                got=True
+        if not got:
+            print('抱歉，查不到!')                                      
     elif sel=='5': # 測驗
-        print('5. 測驗學習成果')
+        score=0
+        print('The total score is', len(d), 'points')
+        for k, v in d.items():
+            print(v, ':')
+            ans = input()
+            if ans == k:    
+                score += 1
+                print('correct! you got', score, 'points now')
+            else:
+                print('wrong! you got', score, 'points now')
     elif sel=='6':
-        print('6. 離開系統')
+        fo.close()
         break
     else:
         print('輸入錯誤，請重新輸入!')
+
+
